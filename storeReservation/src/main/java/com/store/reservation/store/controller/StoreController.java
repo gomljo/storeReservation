2a35b54b80425272b971feeec598d3ab824ca36b
gomljo <gomljo0307@naver.com>
2024-03-26 17:55:14 +0900

    refactor: 매장 컨트롤러 기능 수정 및 추가
    - 기능 수정
      - 매장 정보 등록 시 매장 위치 정보를 얻는 기능의 실행을 서비스 계층으로 이전
        - 매장 정보 수정
          - @PathVariable을 사용하여 매장 id를 받는 방법으로 수정
    
    - 기능 추가
      - 회원 정보 없이 조회
        - 조회 기능은 회원이 아니더라도 매장에 대한 정보는 조회할 수 있도록 구성
          - access token 검증 로직에서 제외
        - 매장 목록 조회
          - 비회원과 회원 모두 검색 가능
          - 가까운 거리 순, 리뷰 갯수 순, 별점 순으로 정렬 기준을 변경 가능
        - 매장 정보 조회(by 모든 사용자(비회원 포함))
           - 매장 세부 정보 조회
      - 회원 정보 기반 조회
         - 매장 정보 조회(by 점장)
          - 매장 소유주(점장)에 의한 매장 정보 조회
            - 점장 권한을 가진 사용자만 조회 가능

--- storeReservation/src/main/java/com/store/reservation/store/controller/StoreController.java
+++ storeReservation/src/main/java/com/store/reservation/store/controller/StoreController.java
@@ -1,13 +1,19 @@
 package com.store.reservation.store.controller;
 
-import com.store.reservation.member.memberInfo.domain.MemberInformation;
-import com.store.reservation.member.memberInfo.model.SecurityUser;
-import com.store.reservation.member.memberInfo.service.MemberService;
-import com.store.reservation.store.dto.create.StoreDto;
+import com.store.reservation.member.domain.MemberInformation;
+import com.store.reservation.member.model.SecurityUser;
+import com.store.reservation.member.service.MemberService;
+import com.store.reservation.store.constant.StoreStatus;
+import com.store.reservation.store.dto.create.CreateStoreDto;
+import com.store.reservation.store.dto.search.request.SearchStoreDto;
+import com.store.reservation.store.dto.search.response.StoreDto;
+import com.store.reservation.store.dto.update.UpdateStoreDto;
+import com.store.reservation.store.repository.queryDsl.dto.SimpleStore;
 import com.store.reservation.store.service.StoreService;
-import com.store.reservation.store.util.GeoCoding;
-import com.store.reservation.store.util.implementation.kakao.dto.LocationDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -20,26 +26,46 @@ public class StoreController {
 
     private final StoreService storeService;
     private final MemberService memberService;
-    private final GeoCoding geoCoding;
+
 
     @PostMapping("/register")
     @PreAuthorize("hasRole('PARTNER')")
-    public void registerStore(
+    public ResponseEntity<StoreStatus> registerStore(
             @AuthenticationPrincipal SecurityUser securityUser,
-            @RequestBody StoreDto storeDto) {
-
+            @RequestBody CreateStoreDto createStoreDto) {
         MemberInformation memberInformation = memberService.searchBy(securityUser.getId());
-        LocationDto locationDto = geoCoding.convertToCoordinateFrom(storeDto.getRoadName());
-        storeService.create(storeDto, locationDto, memberInformation);
+        storeService.create(createStoreDto, memberInformation);
+        return ResponseEntity.ok(StoreStatus.CREATION_SUCCESS);
     }
 
-    @PutMapping("/update")
+    @PutMapping("/update/{storeId}")
     @PreAuthorize("hasRole('PARTNER')")
-    public void updateStore(
+    public ResponseEntity<StoreStatus> updateStore(
             @AuthenticationPrincipal SecurityUser securityUser,
-            @RequestBody StoreDto storeDto) {
+            @PathVariable Long storeId,
+            @RequestBody UpdateStoreDto storeDto) {
         MemberInformation memberInformation = memberService.searchBy(securityUser.getId());
-        LocationDto locationDto = geoCoding.convertToCoordinateFrom(storeDto.getRoadName());
-        storeService.update(storeDto, locationDto, memberInformation);
+        storeService.update(storeDto, storeId, memberInformation);
+        return ResponseEntity.ok(StoreStatus.UPDATE_SUCCESS);
+    }
+
+    @GetMapping("/search/{pageIndex}/{pageSize}")
+    public Page<SimpleStore> searchStoreBy(@RequestParam int pageIndex,
+                                           @RequestParam int pageSize,
+                                           @RequestBody SearchStoreDto searchStoreDto) {
+        return storeService.searchStoreListBy(searchStoreDto, PageRequest.of(pageSize, pageIndex));
+    }
+
+    @GetMapping("/search/manager/detail/{storeId}")
+    @PreAuthorize("hasRole('ROLE_PARTNER')")
+    public ResponseEntity<StoreDto> searchStoreByManager(@AuthenticationPrincipal SecurityUser securityUser,
+                                                @PathVariable Long storeId) {
+        MemberInformation memberInformation = memberService.searchBy(securityUser.getId());
+        return ResponseEntity.ok(StoreDto.from(storeService.searchStoreByOwner(memberInformation, storeId)));
+    }
+
+    @GetMapping("/search/customer/detail/{storeId}")
+    public ResponseEntity<StoreDto> searchStoreByCustomer(@PathVariable Long storeId){
+        return ResponseEntity.ok(StoreDto.from(storeService.searchStoreByCustomer(storeId)));
     }
 }
