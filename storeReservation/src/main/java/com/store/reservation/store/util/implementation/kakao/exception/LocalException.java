fc10679b5dd070b3be472c5a915c6e81a09a41ce
gomljo <gomljo0307@naver.com>
2024-03-26 18:05:13 +0900

    chore: 누락된 파일 업로드

--- storeReservation/src/main/java/com/store/reservation/store/util/implementation/kakao/exception/LocalException.java
+++ storeReservation/src/main/java/com/store/reservation/store/util/implementation/kakao/exception/LocalException.java
@@ -2,21 +2,22 @@ package com.store.reservation.store.util.implementation.kakao.exception;
 
 import com.store.reservation.exception.CustomException;
 
+
 public class LocalException extends CustomException {
 
     private final LocalErrorCode localErrorCode;
 
-    public LocalException(LocalErrorCode localErrorCode){
+    public LocalException(LocalErrorCode localErrorCode) {
         this.localErrorCode = localErrorCode;
     }
 
     @Override
-    protected String getDescription() {
+    public String getDescription() {
         return localErrorCode.getDescription();
     }
 
     @Override
-    protected String getErrorCode() {
+    public String getErrorCode() {
         return localErrorCode.name();
     }
 }
