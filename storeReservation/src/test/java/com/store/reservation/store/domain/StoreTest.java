f257a063764dda25f8eab72660c8098571267830
gomljo <gomljo0307@naver.com>
2024-03-29 00:51:38 +0900

    chore: 이전의 테스트 코드 삭제
    새롭게 변경될 부분이 많아 실행 편의성을 위해 기존의 태스트 코드 삭제

---
+++ storeReservation/src/test/java/com/store/reservation/store/domain/StoreTest.java
@@ -0,0 +1,23 @@
+package com.store.reservation.store.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StoreTest {
+
+//    @Test
+//    @DisplayName("쉬는 시간을 제외하고 ")
+//    void (){
+//
+//        // given
+//
+//
+//        // when
+//    
+//
+//        // then
+//
+//
+//    }
+
+}
