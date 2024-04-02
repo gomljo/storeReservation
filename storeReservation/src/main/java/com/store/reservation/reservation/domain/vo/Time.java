bab75b9ddbd6a4f003111c3fa5c4c76b206b25c8
gomljo <gomljo0307@naver.com>
2024-04-02 23:44:44 +0900

    chore: 예약 엔티티 수정
    - 수정 사항
      - 예약 엔티티 id 값의 이름 변경
      - time 객체 jsonFormat 어노테이션 지정

--- storeReservation/src/main/java/com/store/reservation/reservation/domain/vo/Time.java
+++ storeReservation/src/main/java/com/store/reservation/reservation/domain/vo/Time.java
@@ -1,5 +1,6 @@
 package com.store.reservation.reservation.domain.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,13 +17,13 @@ import java.time.LocalTime;
 @Getter
 @Builder
 public class Time {
-
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "YYYY-MM-dd", timezone = "Asia/Seoul")
     private LocalDate reservedDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "kk:mm", timezone = "Asia/Seoul")
     private LocalTime reservedTime;
 
-    public LocalDateTime getTime(){
+    public LocalDateTime getTime() {
         return LocalDateTime.of(this.reservedDate, this.reservedTime);
     }
-
 
 }
