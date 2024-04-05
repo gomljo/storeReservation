b685fa110b56ce085d6e6da64dac1a02c1636fab
gomljo <gomljo0307@naver.com>
2024-04-05 11:38:54 +0900

    refactor: 필드 설정 값 변경
    - 예약 상태
      - enum type을 String으로 지정하지 않아 order 값으로 저장되어 변경
    - 예약 시간 dto
      - 날짜와 시간 형식 지정

--- storeReservation/src/main/java/com/store/reservation/reservation/dto/common/ReservationTimeDto.java
+++ storeReservation/src/main/java/com/store/reservation/reservation/dto/common/ReservationTimeDto.java
@@ -1,10 +1,12 @@
 package com.store.reservation.reservation.dto.common;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -14,7 +16,8 @@ import java.time.LocalTime;
 @Getter
 @Builder
 public class ReservationTimeDto {
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "YYYY-MM-dd", timezone = "Asia/Seoul")
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate reservationDate;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "kk:mm", timezone = "Asia/Seoul")
     private LocalTime reservationTime;
