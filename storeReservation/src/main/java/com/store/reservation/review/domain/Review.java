4e2fc468a48a32a3e6067652072faa777369560c
gomljo <gomljo0307@naver.com>
2024-04-09 23:49:10 +0900

    refactor: 후기 엔티티 수정
    - 수정 사항
      - 필드 명 수정
        - 이름이 겹쳐 의미를 분명하게 하기 위해 수정
      - 후기 수정 기능 추가

--- storeReservation/src/main/java/com/store/reservation/review/domain/Review.java
+++ storeReservation/src/main/java/com/store/reservation/review/domain/Review.java
@@ -1,6 +1,7 @@
 package com.store.reservation.review.domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.store.reservation.member.domain.MemberInformation;
 import com.store.reservation.member.model.BaseEntity;
 import com.store.reservation.store.domain.model.Store;
 import lombok.*;
@@ -16,16 +17,24 @@ public class Review extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long reviewId;
+    private long id;
 
-    private String review;
+    private String content;
 
     private int starRating;
+
+    @ManyToOne
+    @JoinColumn(name = "member_information_id")
+    private MemberInformation memberInformation;
 
     @JsonBackReference
     @ManyToOne
     @JoinColumn(name = "storeId")
     private Store store;
 
+    public void update(int starRating, String content){
+        this.starRating = starRating;
+        this.content = content;
+    }
 
 }
