5a2a338881ace4a13ba2b4a004ad9800e54e055d
gomljo <gomljo0307@naver.com>
2024-04-14 21:35:26 +0900

    feat: 후기 컨트롤러 구현
    - 구현 사항
      - 후기 작성 api
      - 후기 수정 api
      - 후기 목록 조회 api
        - 고객에 의한 후기 목록 조회 api
        - 매장에 의한 후기 목록 조회 api

--- storeReservation/src/main/java/com/store/reservation/global/review/dto/ReviewDetailDto.java
+++ storeReservation/src/main/java/com/store/reservation/global/review/dto/ReviewDetailDto.java
@@ -1,19 +1,31 @@
 package com.store.reservation.global.review.dto;
 
 import com.store.reservation.review.domain.Review;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
+@Schema(name = "ReviewDetailDto")
 public class ReviewDetailDto {
-
+    @Min(1)
+    @Schema(name = "후기 Id")
     private Long reviewId;
+    @NotNull
+    @Schema(name = "후기 내용")
     private String content;
+    @Min(1)
+    @Max(5)
+    @Schema(name = "별점")
     private Integer starRating;
 
 
--- storeReservation/src/main/java/com/store/reservation/global/review/dto/ReviewSearchDto.java
+++ storeReservation/src/main/java/com/store/reservation/global/review/dto/ReviewSearchDto.java
@@ -2,18 +2,28 @@ package com.store.reservation.global.review.dto;
 
 import com.store.reservation.reservation.constants.search.Duration;
 import com.store.reservation.reservation.constants.search.SortOption;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Pageable;
 
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
+@Schema(name = "ReviewSearchDto")
 public class ReviewSearchDto {
+    @NotNull
+    @Schema(name = "조회 기간")
     private Duration duration;
+    @NotNull
+    @Schema(name = "정렬 순서")
     private SortOption sortOption;
+    @NotNull
+    @Schema(name = "페이징 관련 정보")
     private Pageable pageable;
 }
