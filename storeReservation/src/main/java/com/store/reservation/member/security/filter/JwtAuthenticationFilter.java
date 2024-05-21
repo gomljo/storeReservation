1eb1f4c2f1a0357006698080123f693c758e2e9c
gomljo <gomljo0307@naver.com>
2024-05-21 23:48:53 +0900

    refactor: refresh token 검증 로직 추가
    - 수정 사항
      - 기존에 access token의 유효성만 검사하던 로직을 refresh token도
        검사하도록 수정
      - request header의 토큰 값의 prefix 전처리를 provider로 위임

--- storeReservation/src/main/java/com/store/reservation/member/security/filter/JwtAuthenticationFilter.java
+++ storeReservation/src/main/java/com/store/reservation/member/security/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,9 @@
-package com.store.reservation.member.security;
+package com.store.reservation.member.security.filter;
 
-import lombok.RequiredArgsConstructor;
+import com.store.reservation.member.security.provider.JWTProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,42 +12,39 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @Slf4j
-@Component
-@RequiredArgsConstructor
-public class JwtAuthenticationFilter extends AuthenticationFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private static final String ACCESS_TOKEN_HEADER = "Authorization";
     private static final String REFRESH_TOKEN_HEADER = "RefreshToken";
-    private final TokenProvider tokenProvider;
+    private final JWTProvider jwtProvider;
 
+    public JwtAuthenticationFilter(JWTProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String accessToken = this.resolveTokenFromRequest(request, ACCESS_TOKEN_HEADER);
         String refreshToken = this.resolveTokenFromRequest(request, REFRESH_TOKEN_HEADER);
-        System.out.printf("%s %s\n", "at header: ", request.getHeader(ACCESS_TOKEN_HEADER));
-        System.out.printf("%s %s\n", "rt header: ", request.getHeader(REFRESH_TOKEN_HEADER));
-        System.out.printf("%s %s\n", "at: ", accessToken);
-        System.out.printf("%s %s\n", "rt: ", refreshToken);
 
-        if(StringUtils.hasText(accessToken)){
+        if (StringUtils.hasText(accessToken)) {
             String wantToValidate = accessToken;
-            if(StringUtils.hasText(refreshToken)){
+            if (StringUtils.hasText(refreshToken)) {
                 wantToValidate = refreshToken;
             }
-            this.tokenProvider.validateToken(wantToValidate);
+            this.jwtProvider.validateToken(wantToValidate, new Date());
 
-            Authentication auth = this.tokenProvider.getAuthentication(wantToValidate);
+            Authentication auth = this.jwtProvider.getAuthentication(wantToValidate);
             SecurityContextHolder.getContext().setAuthentication(auth);
-
         }
 
         filterChain.doFilter(request, response);
-    }
 
+    }
     private String resolveTokenFromRequest(HttpServletRequest request, String headerName) {
         return request.getHeader(headerName);
     }
