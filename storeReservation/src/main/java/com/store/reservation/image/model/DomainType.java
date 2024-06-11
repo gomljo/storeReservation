703508a1eefe988676b333e4db4715e4ca37a686
gomljo <gomljo0307@naver.com>
2024-06-11 23:48:28 +0900

    feat: 이미지 엔티티 정의
    - 이미지 엔티티 정의
      - 매장, 리뷰와 회원 도메인에서 사용할 이미지 객체 정의
      - 여러 엔티티와 연관 관계를 맺을 경우 null 값을 허용해야 하는 문제가
        있어 객체와의 연관 관계가 아닌 객체 id 값을 통해 참조할 수 있도록
        구성

---
+++ storeReservation/src/main/java/com/store/reservation/image/model/DomainType.java
@@ -0,0 +1,7 @@
+package com.store.reservation.image.model;
+
+public enum DomainType {
+    STORE,
+    REVIEW,
+    MEMBER
+}
---
+++ storeReservation/src/main/java/com/store/reservation/image/model/UsageType.java
@@ -0,0 +1,11 @@
+package com.store.reservation.image.model;
+
+public enum UsageType {
+
+    STORE_MAIN,
+    STORE_MENU,
+    REVIEW,
+    PROFILE
+    ;
+
+}
